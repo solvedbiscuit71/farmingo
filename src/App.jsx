181604f750ebfcfe96deda5ed6176784a12e75b9
Praveen Perumal <ppraveen98841@gmail.com>
2023-03-25 11:09:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to Farmingo <br />
+        (Farmer-to-Consumer)
+      </h1>
+    </div>
+  )
+}
+
+export default App
